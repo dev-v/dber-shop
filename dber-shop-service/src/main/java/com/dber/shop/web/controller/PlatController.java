69f1c573895cb0a6dd57017e9304dd80d95b65d1
dev-v <250912570@qq.com>
2018-01-18 21:35:45 +0800

    提交

---
+++ dber-shop-service/src/main/java/com/dber/shop/web/controller/PlatController.java
@@ -0,0 +1,19 @@
+package com.dber.shop.web.controller;
+
+import com.dber.base.web.controller.ILoginCheckController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <li>修改记录: ...</li>
+ * <li>内容摘要: ...</li>
+ * <li>其他说明: ...</li>
+ *
+ * @author dev-v
+ * @version 1.0
+ * @since 2018/1/18
+ */
+@RestController
+@RequestMapping("/plat")
+public class PlatController implements ILoginCheckController {
+}
